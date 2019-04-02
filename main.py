5da8d4115670d412d0533a3f64ee880b9b2f0ba1
stephenlasky <stephenjlasky@gmail.com>
2019-04-02 19:55:23 -0400

    Fixed triangle construction bug.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 # This is the main file for the FEM HW # 2
 
 # STEP # 1. INPUTS: f, omega, coefficients
-N  = 16         # this code assumes that N is a real number with a root that is an integer
+N  = 9         # this code assumes that N is a real number with a root that is an integer
 
 number_elements = N * 2
 number_nodes = int( (math.sqrt(N) + 1) ** 2 )
@@ -26,19 +26,21 @@ while i < number_nodes - int(math.sqrt(number_nodes)):      # skip 'last' node v
     a = i
     b = int(math.sqrt(number_nodes)) + a
 
-    # construct upper triangle
+    # construct lower triangle
     nodes_l2g[e, 0] = a
     nodes_l2g[e, 1] = b+1
     nodes_l2g[e, 2] = b
 
-    # construct lower triangle
-    nodes_l2g[e + 1, 0] = 0
+    # construct upper triangle
+    nodes_l2g[e + 1, 0] = a
     nodes_l2g[e + 1, 1] = b+1
     nodes_l2g[e + 1, 2] = a+1
 
     # iteration here
     e += 2
     i += 1
+
+print(nodes_l2g)
 
 # STEP 3. Compute a_K, b_K
 # define function to compute a_k
@@ -78,4 +80,4 @@ for i in range(0,number_elements):
         print_str += x
     print(print_str)
 
-
+print(a)
