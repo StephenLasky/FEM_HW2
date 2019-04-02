3a8a498224abf83149286adbac7983fa358979eb
stephenlasky <stephenjlasky@gmail.com>
2019-04-02 17:06:32 -0400

    First shot at computing a

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print("N:{} Elements:{} Nodes:{}".format(N, number_elements, number_nodes))
 # STEP # 2: Create Th?
 
 # construct nodes_l2g
-nodes_l2g = np.zeros((number_elements, 3)) - 1
+nodes_l2g = np.zeros((number_elements, 3), dtype=int) - 1
 
 i = 0
 e = 0
@@ -39,3 +39,30 @@ while i < number_nodes - int(math.sqrt(number_nodes)):      # skip 'last' node v
     # iteration here
     e += 2
     i += 1
+
+# STEP 3. Compute a_K, b_K
+# define function to compute a_k
+def a_k(i,j):
+    if i == 0 and j == 0: return -1
+    if i == 0 and j == 1: return 2
+    if i == 0 and j == 2: return 2
+
+    if i == 1 and j == 0: return -2
+    if i == 1 and j == 1: return 1
+    if i == 1 and j == 2: return 1
+
+    if i == 2 and j == 0: return -2
+    if i == 2 and j == 1: return 1
+    if i == 2 and j == 2: return 1
+
+a = np.zeros((number_elements, number_elements))
+
+for e in range(0, number_elements):
+    for i in range(0,3):
+        # compute b_k
+
+        for j in range(0,3):
+            # a[nodes_l2g[e,i], nodes_l2g[e,j]] += a_k(i,j)
+            a[nodes_l2g[e, i], nodes_l2g[e, j]] += 1
+
+
